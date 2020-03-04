04aaa667be4c203884fbb56a91fd6f7fc5ce4cd0
pondycrane <pondycrane@gmail.com>
2020-03-04 21:02:54 +0000

    Solve topsort validation problem

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import argparse
 import os
 import importlib
+import json
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('--problem', type=str)
 
+def generate_problem_paths(problem):
+    to_return = {}
+    for folder, appendix in [
+        ('python', '.py'),
+        ('testcases', '.json')
+    ]:
+        to_return[folder] = "{}{}".format(
+            os.path.join(
+                os.path.dirname(
+                    os.path.abspath(__file__)
+                ),
+                folder,
+                *problem
+            ),
+            appendix
+        )
+    return to_return
+
 def run_problem(problem):
-    problem_path = os.path.join(
-        os.path.dirname(
-            os.path.abspath(__file__)
-        ),
-        'python',
-        *problem
-    )
+    problem_paths = generate_problem_paths(problem)
+    script_path, testcase_path = problem_paths['python'], problem_paths['testcases']
+
+    with open(testcase_path) as sh:
+        testcases = json.loads(sh.read())
+    
     spec = importlib.util.spec_from_file_location(
-        "module.name", f"{problem_path}.py"
+        "module.name", f"{script_path}"
     )
     solution = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(solution)
+    solution_obj = solution.Solution(problem)
+    
+    for testcase in testcases:
+        output = getattr(solution_obj, problem[-1])(*testcase[:-1])
+        assert output == testcase[-1], f"output = {output} != {answer}"
 
 if __name__ == '__main__':
     args = parser.parse_args()
--- python/topsort/topsort_basic.py
+++ python/topsort/topsort_basic.py
@@ -1,5 +1,33 @@
+import collections
+
 import base.solution
 
 class Solution(base.solution.Solution):
-    def topsort_basic(self):
-        print("Running topsort basic")
+    
+    def topsort_basic(self, data):
+        graph = collections.defaultdict(list)
+        for a, b in data:
+            graph[a].append(b)
+        
+        # Track total visit, just like regular topsort
+        visited = set()
+
+        def dfs(curr_visited, cur_visit):
+            if cur_visit not in graph:
+                return True
+                
+            for next_visit in graph[cur_visit]:
+                if next_visit in curr_visited:
+                    return False
+                
+                if next_visit not in visited:
+                    curr_visited.add(next_visit)
+                    if not dfs(curr_visited, next_visit):
+                        return False
+            return True
+
+        for course in graph:
+            if course not in visited:
+                if not dfs(set([course]), course): # Use set to track loop
+                    return False
+        return True
