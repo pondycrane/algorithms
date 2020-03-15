835881b8528e571191051c690114eb5a65acadbf
pondycrane <pondycrane@gmail.com>
2020-03-15 16:53:12 +0000

    optimize word ladder

--- python/bfs/word_ladder.py
+++ python/bfs/word_ladder.py
@@ -12,18 +12,18 @@ class Solution(base.solution.Solution):
             for i in range(len(word)):
                 graph[word[0:i] + '*' + word[i + 1:]].append(word)
 
-        queue = collections.deque([(beginWord, 1, '***')])
+        queue = collections.deque([(beginWord, 1)])
         while queue:
-            word, cost, prev = queue.popleft()
+            word, cost = queue.popleft()
             visited[word] = True
             for i in range(len(word)):
                 key = word[0:i] + '*' + word[i + 1:]
-                if key != prev:
-                    for n_word in graph[key]:
-                        if n_word == endWord:
-                            return cost + 1
-                        
-                        if n_word != word and not visited[n_word]:
-                            queue.append((n_word, cost + 1, key))
+                for n_word in graph[key]:
+                    if n_word == endWord:
+                        return cost + 1
+                    
+                    if n_word != word and not visited[n_word]:
+                        queue.append((n_word, cost + 1))
+                graph[key] = []
         return 0
         
