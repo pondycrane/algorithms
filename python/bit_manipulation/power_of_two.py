f4e0edff21fd14cf1f3cb9040688da1c12f3c552
Ho-Chih Huang <pondycrane@gmail.com>
2020-06-08 15:00:35 +0000

    With a better solution

--- python/bit_manipulation/power_of_two.py
+++ python/bit_manipulation/power_of_two.py
@@ -1,6 +1,9 @@
 import base.solution
 
 class Solution(base.solution.Solution):
+    """
+    T: O(N)
+    """
     def power_of_two(self, n: int) -> bool:
         if n <= 0:
             return False
@@ -10,6 +13,18 @@ class Solution(base.solution.Solution):
             one_count += n & 1
             n >>= 1
         return one_count == 1
+    
+    """
+    Turn the right-most bit to 0: Since the solution only has
+    1 bit, turnning the right-most bit to 0 leads to 0 value.
+    x & (x - 1)
+
+    T: O(1)
+    """
+    def power_of_two(self, n: int) -> bool:
+        if n == 0:
+            return False
+        return n & (n - 1) == 0
 
 
 
