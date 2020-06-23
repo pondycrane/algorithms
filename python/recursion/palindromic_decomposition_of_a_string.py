207f2d017baab34731641814e0096ed465d4ba44
Ho-Chih Huang <pondycrane@gmail.com>
2020-06-23 03:12:17 +0000

    Added DP

--- python/recursion/palindromic_decomposition_of_a_string.py
+++ python/recursion/palindromic_decomposition_of_a_string.py
@@ -37,3 +37,40 @@ class Solution(base.solution.Solution):
             
         recursion('', 0)
         return result
+
+    """
+    DP solution, and use list rather than string
+    """
+    def palindromic_decomposition_of_a_string(self, s: str) -> typing.List[str]:
+        result = []
+
+        memo = {}
+        def ispalindrom(left, right):
+            key = (left, right)
+            if key in memo:
+                return memo[key]
+            
+            while left < right:
+                if s[left] != s[right]:
+                    memo[key] = False
+                    return memo[key]
+                left += 1
+                right -= 1
+            memo[key] = True
+            return memo[key]
+        
+        def recursion(slate, pos):
+            # Basecase
+            if pos == len(s):
+                result.append('|'.join(slate))
+                return
+            
+            for j in range(pos, len(s)):
+                # Backtracking
+                if ispalindrom(pos, j):
+                    slate.append(s[pos:j + 1])
+                    recursion(slate, j + 1)
+                    slate.pop()
+            
+        recursion([], 0)
+        return result
