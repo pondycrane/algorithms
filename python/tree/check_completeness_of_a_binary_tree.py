a71a41dca4d1514a4ec352af57d76e63a84a906d
Hank <pondycrane@gmail.com>
2020-07-01 16:07:55 -0400

    check completeness

---
+++ python/tree/check_completeness_of_a_binary_tree.py
@@ -0,0 +1,49 @@
+import collections
+import typing
+
+import base.solution
+from lib.ds_collections.treenode import TreeNode
+
+class Solution(base.solution.Solution):
+    def check_completeness_of_a_binary_tree(self, root: typing.List[int]) -> typing.List[int]:
+        root = TreeNode.deserialize(root)
+
+        if root is None: return True
+        q = collections.deque([root])
+        while q:
+            n = q.popleft()
+            if n is None: break
+            q.append(n.left)
+            q.append(n.right)
+        return not any(q)
+
+"""
+958. Check Completeness of a Binary Tree
+Medium
+
+Given a binary tree, determine if it is a complete binary tree.
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+ 
+
+Example 1:
+
+
+
+Input: [1,2,3,4,5,6]
+Output: true
+Explanation: Every level before the last is full (ie. levels with node-values {1} and {2, 3}), and all nodes in the last level ({4, 5, 6}) are as far left as possible.
+Example 2:
+
+
+
+Input: [1,2,3,4,5,null,7]
+Output: false
+Explanation: The node with value 7 isn't as far left as possible.
+ 
+Note:
+
+The tree will have between 1 and 100 nodes.
+"""
