1e2bdd355eae867ff4e176db870896066a909b5e
Hank <pondycrane@gmail.com>
2020-07-06 14:32:45 -0400

    code the graph solution

--- python/graph/verify_char_crossing.py
+++ python/graph/verify_char_crossing.py
@@ -34,4 +34,33 @@ class Solution(base.solution.Solution):
     Intuition. Need to identify the key relationship between chars that makes this problem a graph problem.
     """
     def verify_char_crossing(self, chars: str) -> bool:
+        indmap = collections.defaultdict(list)
+        graph = collections.defaultdict(list)
+        for i, c in enumerate(chars):
+            indmap[c].append(i)
+        
+        for c, (s, e) in indmap.items():
+            # Get the char between start, end ind with count == 1
+            graph[c] = [cn for cn, count in collections.Counter(chars[s + 1: e]).items() if count == 1]
+        if not graph:
+            return True
+
+        # Start bfs
+        starter = list(graph.keys())[0]
+        q = collections.deque([starter])
+        state = {starter: 1}
+        while q:
+            c = q.popleft()
+            for nc in graph[c]:
+                if nc in state:
+                    if state[nc] != (1 - state[c]):
+                        return False
+                else:
+                    state[nc] = (1 - state[c])
+                    q.append(nc)
+        return True
+
+
+
+
 
