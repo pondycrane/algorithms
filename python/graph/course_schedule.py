76f4584a7063a578150aecf39f5790cb4bb2f351
Hank <pondycrane@gmail.com>
2020-07-08 16:24:44 -0400

    use topsort to solve source schedule problem

--- python/graph/course_schedule.py
+++ python/graph/course_schedule.py
@@ -31,3 +31,27 @@ class Solution(base.solution.Solution):
                     q.append(nv)
                     del depmap[nv]
         return len(depmap) == 0
+
+    """
+    Topsort
+    """
+    def course_schedule(self, dependencies: List[List[int]] = []):
+        g = collections.defaultdict(list)
+        for a, b in dependencies:
+            g[a].append(b)
+            g[b]
+        
+        def dfs(v):
+            if visited[v] == 1: return True
+            if visited[v] == -1: return False
+            visited[v] = -1
+            for nv in g[v]:
+                if not dfs(nv): return False
+            visited[v] = 1
+            return True
+        
+        visited = collections.defaultdict(int)
+        for v in g:
+            if not dfs(v): return False
+
+        return True
