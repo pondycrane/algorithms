5be139b8ec2a1843ec59f92a63bbd1b54bad083e
Hank <pondycrane@gmail.com>
2020-07-13 16:20:27 -0400

    evaluate divisions

--- python/graph/dfs/evaluate_division.py
+++ python/graph/dfs/evaluate_division.py
@@ -8,7 +8,7 @@ class Solution(base.solution.Solution):
     """
     DFS top down
     """
-    def evaluate_division(self, equations: List[List[str]], values: List[float], equations: List[List[str]]) -> List[float]:
+    def evaluate_division(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         g = collections.defaultdict(lambda: collections.defaultdict(float))
         for i, (s, d) in enumerate(equations):
             g[d][s] = values[i]
@@ -35,11 +35,52 @@ class Solution(base.solution.Solution):
     """
     DFS bottom up
     """
-    def evaluate_division(self, equations: List[List[str]], values: List[float], equations: List[List[str]]) -> List[float]:
+    def evaluate_division(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         g = collections.defaultdict(lambda: collections.defaultdict(float))
         for i, (s, d) in enumerate(equations):
             g[d][s] = values[i]
             g[s][d] = 1 / values[i]
 
-        def dfs(v, target, cur, visited):
+        def dfs(v, target):
+            if v in visited:
+                return -1
+            visited.add(v)
+            to_return = -1
+            for nv, nw in g[v].items():
+                if nv == target:
+                    to_return = nw
+                    break
+                cures = dfs(nv, target)
+                if cures != -1:
+                    to_return = nw * cures
+                    break
+            visited.remove(v)
+            return to_return
+
+        res = []
+        visited = set()
+        for d, s in queries:
+            res.append(dfs(s, d))
+        return res
+"""
+399. Evaluate Division
+Medium
+
+Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
+
+Example:
+Given a / b = 2.0, b / c = 3.0.
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
+return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+
+The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries , where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
+
+According to the example above:
+
+equations = [ ["a", "b"], ["b", "c"] ],
+values = [2.0, 3.0],
+queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]. 
  
+
+The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
+"""
